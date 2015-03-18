bdb43e6cd299c45d365a81755c8c9d6e7abf0f6d
lukazz89 <lukazz89@gmail.com>
2015-03-18 18:52:59 +0100

    Init Commit

---
+++ Sztuczna Inteligencja Grafy 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sztuczna_Inteligencja_Grafy_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
