1f2ec87739ae70c84ea70f54f60240d36563a750
lukazz89 <lukazz89@gmail.com>
2015-03-20 18:25:48 +0100

    Data ready

---
+++ Sztuczna Inteligencja Grafy 2/Dane.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace do Sruktury typu Clolr
+using System.Drawing;
+
+namespace Sztuczna_Inteligencja_Grafy_2
+{
+    public static class Dane
+    {
+        public static ushort[,] MacierzSasiedztwa = new ushort[11, 11]
+        {
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0},
+            {0,1,0,0,0,0,0,0,1,0,0}
+        };
+
+        public static Color[] TablicaKolorow = new Color[5]
+        {
+            Color.Blue,
+            Color.Red,
+            Color.Green,
+            Color.Yellow,
+            Color.White
+        };
+
+    }
+}
